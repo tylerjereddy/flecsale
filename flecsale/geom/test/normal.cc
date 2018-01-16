b22e9fd34028c7c0e269b5dd0ad15787570f2192
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-01-16 13:25:54 -0700

    TST: First draft of normal.cc test module.

---
+++ flecsale/geom/test/normal.cc
@@ -0,0 +1,65 @@
+/*~-------------------------------------------------------------------------~~*
+ * Copyright (c) 2018 Los Alamos National Laboratory, LLC
+ * All rights reserved
+ *~-------------------------------------------------------------------------~~*/
+////////////////////////////////////////////////////////////////////////////////
+///
+/// \file
+/// 
+/// \brief Tests related to the normal operation.
+///
+////////////////////////////////////////////////////////////////////////////////
+
+// preprocessor directive for exception handling
+#define ENABLE_EXCEPTIONS
+
+// system includes
+#include<cinchtest.h>
+#include<vector>
+
+// user includes
+#include "flecsale/common/types.h"
+#include "flecsale/geom/normal.h"
+#include "flecsale/geom/point.h"
+
+// explicitly use some stuff
+using std::cout;
+using std::endl;
+using std::vector;
+
+using namespace flecsale;
+using namespace flecsale::geom;
+
+//! the real type
+using real_t = common::real_t;
+// ! the test tolerance
+using common::test_tolerance;
+
+//! the point types
+using point_2d_t = point<real_t, 2>;
+
+//! the error types used
+using utils::ExceptionNotImplemented;
+
+//=============================================================================
+//! \brief Test 2d normal operator.
+//=============================================================================
+TEST(normal, 2d) 
+{
+
+  // 2D normal is (-dy, dx), (dy, -dx)
+  // The library only returns
+  // the first of the above values
+
+  vector<point_2d_t> simple_points = { {0, 0}, {1, 0} };
+  
+  auto result_1 = normal( simple_points );
+
+  // NOTE: the Google Mock C++ framework could be used to directly
+  // check array values in a single statement
+  // see: https://stackoverflow.com/a/2797990/2942522
+  // For now, index array data directly
+  ASSERT_NEAR( 0.0, result_1[0], test_tolerance ) << " 2D normal calculation wrong ";
+  ASSERT_NEAR( 1.0, result_1[1], test_tolerance ) << " 2D normal calculation wrong ";
+
+} // TEST
